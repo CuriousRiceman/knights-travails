08e1e76380b7f4ccc2097fae0b54a9a91441a225
Richman <richmanong4@gmail.com>
2024-08-24 14:41:24 -1000

    Used adjacency list structure, spent days on DFS, just to settle for BFS, extremely bummed out

--- knights.js
+++ knights.js
@@ -20,38 +20,73 @@ function isCoordinateInArray(array, coordinate) {
   return array.some(([x, y]) => x === coordinate[0] && y === coordinate[1]);
 }
 
-function knightMoves(start, end) {
-  let moves = possibleMoves(start);
-  let queue = [ [start] ];
+function generateList(start, end) {
+  let queue = [start];
+  let adjacencyList = [];
   let visited = [];
-
   while (queue.length !== 0) {
-    console.log(queue);
-    let specificPath = queue.shift();
+    let specificMove = queue.shift();
+    let possibleMove = possibleMoves(specificMove);
+    
+    if (!isCoordinateInArray(visited, specificMove)) {
+      visited.push(specificMove);
+      adjacencyList.push([ specificMove, [...possibleMove] ])
 
-    let previousPosition = specificPath[specificPath.length - 1];
-    //console.log(previousPosition)
+      if (isCoordinateInArray(possibleMove, end)) {
+        break;
+      } else {
+        for (let i = 0; i < possibleMove.length; i++) {
+          if (!isCoordinateInArray(visited, possibleMove[i])) {
+            queue.push(possibleMove[i]);
+          } else {
+            continue;
+          }
+        }
+      }
 
-    if (previousPosition[0] === end[0] && previousPosition[1] === end[1]) {
-      console.log("FOUND ONE");
-      return specificPath;
+    } else {
+      continue;
+    }
+  }
+  return adjacencyList;
+}
+
+function checkForPath(adjacencyList, start, end) {
+  let path = [];
+  let queue = [[start]]; // Queue of paths
+  let visited = new Set();
+  visited.add(start.toString());
+
+  while (queue.length > 0) {
+    let currentPath = queue.shift(); // Get the current path
+    let currentNode = currentPath[currentPath.length - 1]; // Last node in the path
+
+    if (currentNode.toString() === end.toString()) {
+      return currentPath; // Found the path to the end
     }
 
-    if (!isCoordinateInArray(visited, previousPosition)) {
-      visited.push(previousPosition);
-
-      let moreMoves = possibleMoves(previousPosition);
-
-      for (let i = 0; i < moreMoves.length; i++) {
-        if (!isCoordinateInArray(visited, moreMoves[i])) {
-          queue.push([...specificPath, moreMoves[i]]);
+    for (let [node, neighbors] of adjacencyList) {
+      if (node.toString() === currentNode.toString()) {
+        for (let neighbor of neighbors) {
+          if (!visited.has(neighbor.toString())) {
+            visited.add(neighbor.toString());
+            queue.push([...currentPath, neighbor]); // Extend the path and add to queue
+          }
         }
       }
     }
   }
 
+  return []; // No path found
 }
 
-let start = [0,0];
-let end = [7,7];
+function knightMoves(start, end) {
+  let generatedList = generateList(start, end);
+  let path = checkForPath(generatedList, start, end)
+  return path;
+}
+
+
+let start = [3,3];
+let end = [3,4];
 console.log(knightMoves(start, end));
